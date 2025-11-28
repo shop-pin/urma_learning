25675a1737508eaef6a50aa88ddf1b8a49fca2f0
yczhao <ycchaocau@sina.cn>
2025-11-28 15:16:02 +0800

    cam: add normal combine kernel for A3

---
+++ src/cam/comm_operator/ascend_kernels/moe_combine_normal/op_kernel/cam_moe_combine_normal_tiling.h
@@ -0,0 +1,42 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ * Description: Normal combine tiling header file
+ * Create: 2025-11-28
+ * Note:
+ * History: 2025-11-28 create normal combine tiling header file
+ */
+
+#ifndef CAM_MOE_COMBINE_NORMAL_TILING_H
+#define CAM_MOE_COMBINE_NORMAL_TILING_H
+
+#include <cstdint>
+#include "kernel_tiling/kernel_tiling.h"
+
+struct CamMoeCombineNormalInfo {
+    uint32_t epWorldSize;
+    uint32_t tpWorldSize;
+    uint32_t epRankId;
+    uint32_t tpRankId;
+    uint32_t expertShardType;
+    uint32_t moeExpertNum;
+    uint32_t moeExpertPerRankNum;
+    uint32_t globalBs;
+    uint32_t bs;
+    uint32_t k;
+    uint32_t h;
+    uint32_t aivNum;
+    uint64_t totalUbSize;
+    uint64_t totalWinSize;
+    float armAvgFactor;
+    float epsilon;
+    bool isEnableDiagnose;
+};
+struct CamMoeCombineNormalTilingData {
+    Mc2InitTiling mc2InitTiling;
+    Mc2CcTiling mc2CcTiling1;
+    Mc2CcTiling mc2CcTiling2;
+    CamMoeCombineNormalInfo camMoeCombineNormalInfo;
+};
+
+#endif  // CAM_MOE_COMBINE_NORMAL_TILING_H
