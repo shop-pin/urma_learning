ad64a8e77b3d7e3641f9be69370de880d6b4fba3
lee-yuxing <liyuxing5@huawei.com>
2025-12-11 15:38:39 +0800

    cam: fix pybind fuseddeepmoe whl packet compile issue

--- build/cam/comm_operator/build_pybind.sh
+++ build/cam/comm_operator/build_pybind.sh
@@ -7,63 +7,29 @@
 # History: 2025-12-09 create pybind building script
 
 set -e
-EXT_PATH=${MODULE_BUILD_PATH}/deep_ep
+EXT_PATH=$MODULE_BUILD_PATH/pybind/
 DIST_OUT_PATH=$MODULE_BUILD_OUT_PATH
-DIST_GEN_PATH=$EXT_PATH/output
 
-OUTPUT_DIR=${MODULE_BUILD_PATH}/deep_ep/output
-echo "outpath: ${OUTPUT_DIR}"
+if [ ! -d "$MODULE_BUILD_PATH" ]; then
+    mkdir -p $MODULE_BUILD_PATH
+fi
 
-COMPILE_OPTIONS=""
-
-build_deepep()
-{
-    BUILD_DIR="build"
-
-    if [ -d "$BUILD_DIR" ]; then
-        rm -rf $BUILD_DIR
+build_pybind() {
+    if [ -d "$DIST_OUT_PATH/dist" ]; then
+        rm -rf $DIST_OUT_PATH/dist
     fi
-    mkdir -p $BUILD_DIR
-
-    cmake $COMPILE_OPTIONS -DCMAKE_INSTALL_PREFIX="$OUTPUT_DIR" -DASCEND_HOME_PATH=$ASCEND_HOME_PATH -B "$BUILD_DIR" -S .
-    cmake --build "$BUILD_DIR" -j8 && cmake --build "$BUILD_DIR" --target install
-}
-
-make_deepep_package()
-{
-    if pip3 show wheel;then
-        echo "wheel has been installed"
-    else
-        pip3 install wheel
-    fi
-
-    PYTHON_DIR="python"
-    cd "$PYTHON_DIR" || exit
-
-    cp -v ${OUTPUT_DIR}/lib/* "$EXT_PATH"/python/deep_ep/
-    if [ -d "${EXT_PATH}/python/dist" ]; then
-        rm -rf ${EXT_PATH}/python/dist
-    fi
-    python3 setup.py bdist_wheel
-    mv -v "$EXT_PATH"/python/dist/umdk_cam_operator_normal*.whl ${OUTPUT_DIR}/
-}
-
-build_pybind()
-{
-    if [ -d "$$MODULE_SRC_PATH/pybind/deep_ep" ]; then
-        rm -rf $MODULE_SRC_PATH/pybind/deep_ep
-    fi
-    cp -rf $MODULE_SRC_PATH/pybind/deep_ep $MODULE_BUILD_PATH
-    cp -rf $MODULE_SRC_PATH/pybind/pytorch_extension $MODULE_BUILD_PATH
+    cp -rf $MODULE_SRC_PATH/pybind $MODULE_BUILD_PATH
+    cp -rf $MODULE_SRC_PATH/pybind/pytorch_extension $BUILD_PATH
     cd $EXT_PATH
-    build_deepep
-    make_deepep_package
+    python3 setup.py bdist_wheel
+    DIST_GEN_PATH=$EXT_PATH/dist/
     if [ -d "$DIST_GEN_PATH" ]; then
         echo "copy $DIST_GEN_PATH to $DIST_OUT_PATH/"
-        cp -rf $DIST_GEN_PATH/umdk_cam_operator_normal*.whl $DIST_OUT_PATH/
+        cp -rf $DIST_GEN_PATH $DIST_OUT_PATH
     else
         echo $DIST_GEN_PATH does not exist
         echo "build_pybind fail"
+        return 1
     fi
 }
 
---
+++ src/cam/comm_operator/pybind/setup.py
@@ -0,0 +1,100 @@
+#
+# SPDX-License-Identifier: MIT
+# Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+# Description: pybind setup file
+# Create: 2025-12-11
+# Note:
+# History: 2025-12-11 create pybind setup file
+#
+
+import os
+import sys
+import torch
+import platform
+import importlib.util
+
+sys.path.append(os.path.join(os.path.dirname(__file__), './pytorch_extension'))
+from bdist_wheel_build import BdistWheelBuild
+from setuptools import setup, find_packages
+from torch.utils.cpp_extension import BuildExtension
+from torch_npu.utils.cpp_extension import NpuExtension
+
+# 格式: V版本.R版本.C版本.B版本
+env_version = os.getenv('CAM_WHL_VERSION', '208.1.0.B001')
+
+torch_path = os.path.dirname(torch.__file__)
+torch_npu_spec = importlib.util.find_spec('torch_npu')
+torch_npu_path = os.path.dirname(torch_npu_spec.origin)
+print(f"torch_path: {torch_path}")
+print(f"torch_npu_path: {torch_npu_path}")
+PYTORCH_NPU_INSTALL_PATH = os.path.dirname(os.path.abspath(torch_npu_spec.origin))
+architecture = str(platform.machine())
+if architecture.startswith('x86'):
+    arch = "x86_64"
+else:
+    arch = "aarch64"
+
+env_names = ["ASCEND_HOME_PATH"]
+for env_name in env_names:
+    if env_name not in os.environ:
+        print(f"{env_name} is not in env, please export {env_name} first")
+compile_args = [
+    '-I' + os.path.join(PYTORCH_NPU_INSTALL_PATH, "include/third_party/acl/inc"),
+    "-fPIC", "-fstack-protector-strong", "-w",
+    "-D_FORTIFY_SOURCE=2",
+]
+if "BUILD_TYPE" in os.environ and os.environ.get("BUILD_TYPE") == "Debug":
+    compile_args.extend(['-g', '-O0'])
+else:
+    compile_args.extend(['-O2'])
+if "ENABLE_COV" in os.environ and os.environ.get("ENABLE_COV") == "1":
+    compile_args.extend(['-coverage'])
+print(compile_args)
+
+exts = []
+ext1 = NpuExtension(
+    name="cam_ge_op_lib",
+    include_dirs=[
+        os.path.join(torch_npu_path, 'include'),
+        os.path.join(torch_npu_path, 'include/third_party/acl/inc/acl/'),
+        os.path.join(torch_npu_path, 'include/third_party/acl/inc'),
+        os.path.join(os.environ["ASCEND_HOME_PATH"], f'{arch}-linux', 'include'),
+        os.path.join(os.environ["ASCEND_HOME_PATH"], f'{arch}-linux', 'include', 'hccl'),
+        os.path.join(os.environ["ASCEND_HOME_PATH"], f'{arch}-linux', 'include', 'experiment', 'runtime'),
+        os.path.join(os.environ["ASCEND_HOME_PATH"], f'{arch}-linux', 'include', "experiment", 'msprof'),
+        os.path.join(torch_path, 'include'),
+        os.path.join(os.path.dirname(__file__), "./", "pytorch_extension")],
+
+    library_dirs=[
+        os.path.join(torch_path, 'lib'),
+        os.path.join(torch_npu_path, 'lib'),
+        os.path.join(os.environ["ASCEND_HOME_PATH"], 'opp', 'vendors', 'CAM', 'op_api', 'lib'),
+        os.path.join(os.environ["ASCEND_HOME_PATH"], f'{arch}-linux', 'lib64')],
+    libraries=[
+        "torch_npu",
+        'gcov',
+        'runtime', 'torch', 'ascendcl', 'profapi', 'opapi', 'cust_opapi'],
+    sources=["./fused_deep_moe.cpp",
+            "./pybind.cpp",
+            ],
+    
+    extra_compile_args = compile_args,
+    extra_link_args = [
+        "-s", "-Wl,-z,relro,-z,now"
+    ],
+)
+
+exts.append(ext1)
+BdistWheelBuild.dependencies = ["libc10.so", "libtorch.so", "libtorch_cpu.so", "libtorch_python.so", "libtorch_npu.so"]
+
+setup(
+    name="cam_ge_operator",
+    version=env_version,
+    keywords='cam_ge_op_lib',
+    ext_modules=exts,
+    packages=find_packages(),
+    cmdclass={
+        "build_ext": BuildExtension,
+        'bdist_wheel': BdistWheelBuild
+    },
+)
