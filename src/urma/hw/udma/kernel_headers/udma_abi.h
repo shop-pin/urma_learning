shop-pin/urma_learning

commit 30ea7621dafb452a9d969503c8b5357f32e63de0

udma: support 2M hugepage
category: feature
bugzilla: https://gitee.com/openeuler/umdk/issues/ID45DE
CVE: NA

-----------------------------------------------------------

The Jetty queue buffer and RC table buffer support memory
allocation in 2MB increments to optimize performance.

Signed-off-by: Jiahao Zhang <zhangjiahao14@huawei.com>

src/urma/hw/udma/udma_u_buf.c
@@ -32,7 +32,18 @@ bool udma_u_alloc_queue_buf(struct udma_u_jetty_queue *q, uint32_t max_entry_cnt
 		}
 	}
 
-	q->qbuf = udma_u_alloc_buf(q->qbuf_size);
+	if (q->ctx->hugepage_enable) {
+		q->hugepage = udma_u_alloc_hugepage(q->ctx, q->qbuf_size);
+		if (q->hugepage) {
+			q->qbuf = q->hugepage->va_start;
+		} else {
+			UDMA_LOG_WARN("failed to alloc hugepage buf, switch to alloc normal buf.");
+			q->qbuf = udma_u_alloc_buf(q->qbuf_size);
+		}
+	} else {
+		q->qbuf = udma_u_alloc_buf(q->qbuf_size);
+	}
+
 	if (!q->qbuf) {
 		UDMA_LOG_ERR("failed to alloc queue buffer.\n");
 		if (wrid_en) {
@@ -58,7 +69,10 @@ void udma_u_free_queue_buf(struct udma_u_jetty_queue *q)
 		return;
 
 	if (q->qbuf != NULL) {
-		udma_u_free_buf(q->qbuf, q->qbuf_size);
+		if (q->hugepage)
+			udma_u_free_hugepage(q->ctx, q->hugepage);
+		else
+			udma_u_free_buf(q->qbuf, q->qbuf_size);
 		q->qbuf = NULL;
 		q->qbuf_curr = NULL;
 		q->qbuf_end = NULL;
@@ -85,4 +99,137 @@ void *udma_u_alloc_buf(uint32_t buf_size)
 	}
 
 	return buf;
+}
+
+static void udma_u_hugepage_add(struct udma_u_context *ctx,
+				struct udma_u_hugepage_priv *priv)
+{
+	if (!ctx->hugepage_list) {
+		ctx->hugepage_list = priv;
+		priv->next = NULL;
+	} else {
+		priv->next = ctx->hugepage_list;
+		ctx->hugepage_list->pre = priv;
+		ctx->hugepage_list = priv;
+	}
+	priv->pre = NULL;
+}
+
+static void udma_u_hugepage_del(struct udma_u_context *ctx,
+				struct udma_u_hugepage_priv *priv)
+{
+	if (priv->pre)
+		priv->pre->next = priv->next;
+	else
+		ctx->hugepage_list = priv->next;
+
+	if (priv->next)
+		priv->next->pre = priv->pre;
+}
+
+static struct udma_u_hugepage_priv *
+udma_u_alloc_hugepage_priv(struct udma_u_context *ctx, uint32_t len)
+{
+	off_t offset = get_mmap_offset(0, ctx->page_size, UDMA_MMAP_HUGEPAGE);
+	struct udma_u_hugepage_priv *priv;
+	int ret;
+
+	priv = (struct udma_u_hugepage_priv *)calloc(1, sizeof(*priv));
+	if (!priv) {
+		UDMA_LOG_ERR("alloc hugepage_priv failed.\n");
+		return NULL;
+	}
+
+	priv->va_len = align(len, UDMA_HUGEPAGE_SIZE);
+	priv->left_va_len = priv->va_len;
+	priv->va_base = mmap(NULL, priv->va_len, PROT_READ | PROT_WRITE,
+			    MAP_SHARED, ctx->urma_ctx.dev_fd, offset);
+	if (priv->va_base == MAP_FAILED) {
+		UDMA_LOG_ERR("mmap failed, buf_size=%u.\n", priv->va_len);
+		goto err_mmap;
+	}
+
+	ret = madvise(priv->va_base, priv->va_len, MADV_DONTFORK);
+	if (ret) {
+		UDMA_LOG_ERR("buf madvise failed! ret = %d\n", ret);
+		goto err_madvise;
+	}
+	udma_u_hugepage_add(ctx, priv);
+
+	return priv;
+
+err_madvise:
+	(void)munmap(priv->va_base, priv->va_len);
+err_mmap:
+	free(priv);
+
+	return NULL;
+}
+
+struct udma_u_hugepage *udma_u_alloc_hugepage(struct udma_u_context *ctx, uint32_t len)
+{
+	struct udma_u_hugepage_priv *priv = NULL;
+	struct udma_u_hugepage *hugepage;
+
+	hugepage = (struct udma_u_hugepage *)calloc(1, sizeof(*hugepage));
+	if (!hugepage) {
+		UDMA_LOG_ERR("alloc hugepage failed.\n");
+		return NULL;
+	}
+
+	pthread_mutex_lock(&ctx->hugepage_lock);
+	if (ctx->hugepage_list && ctx->hugepage_list->left_va_len >= len)
+		priv = ctx->hugepage_list;
+
+	if (!priv) {
+		priv = udma_u_alloc_hugepage_priv(ctx, len);
+		if (!priv) {
+			pthread_mutex_unlock(&ctx->hugepage_lock);
+			free(hugepage);
+			return NULL;
+		}
+	}
+
+	hugepage->va_start = priv->va_base + priv->left_va_offset;
+	hugepage->va_len = len;
+	hugepage->priv = priv;
+	priv->left_va_offset += len;
+	priv->left_va_len -= len;
+	priv->refcnt++;
+	pthread_mutex_unlock(&ctx->hugepage_lock);
+
+	return hugepage;
+}
+
+void udma_u_free_hugepage(struct udma_u_context *ctx, struct udma_u_hugepage *hugepage)
+{
+	pthread_mutex_lock(&ctx->hugepage_lock);
+	hugepage->priv->refcnt--;
+	if (hugepage->priv->refcnt) {
+		pthread_mutex_unlock(&ctx->hugepage_lock);
+		free(hugepage);
+		return;
+	}
+
+	(void)munmap(hugepage->priv->va_base, hugepage->priv->va_len);
+	udma_u_hugepage_del(ctx, hugepage->priv);
+	pthread_mutex_unlock(&ctx->hugepage_lock);
+
+	free(hugepage->priv);
+	free(hugepage);
+}
+
+void udma_u_destroy_hugepage(struct udma_u_context *ctx)
+{
+	struct udma_u_hugepage_priv *priv;
+
+	pthread_mutex_lock(&ctx->hugepage_lock);
+	while (ctx->hugepage_list) {
+		priv = ctx->hugepage_list;
+		udma_u_hugepage_del(ctx, priv);
+		(void)munmap(priv->va_base, priv->va_len);
+		free(priv);
+	}
+	pthread_mutex_unlock(&ctx->hugepage_lock);
+	(void)pthread_mutex_destroy(&ctx->hugepage_lock);
 }

src/urma/hw/udma/udma_u_buf.h
@@ -23,4 +23,8 @@ static inline void udma_u_free_buf(void *buf, uint32_t buf_size)
 	(void)munmap(buf, buf_size);
 }
 
+struct udma_u_hugepage *udma_u_alloc_hugepage(struct udma_u_context *ctx, uint32_t len);
+void udma_u_free_hugepage(struct udma_u_context *ctx, struct udma_u_hugepage *hugepage);
+void udma_u_destroy_hugepage(struct udma_u_context *ctx);
+
 #endif /* __UDMA_U_BUF_H__ */

src/urma/hw/udma/udma_u_common.h
@@ -30,6 +30,7 @@
 #define UDMA_MIN_JFS_WQEBB_CNT 64
 #define UDMA_MIN_CCU_WQEBB_CNT 16
 #define UDMA_MAX_JFS_WQEBB_CNT 32768
+#define UDMA_HUGEPAGE_SIZE 2097152
 #define UDMA_HW_PAGE_SIZE 4096U
 #define UDMA_BITS_PER_LONG 64
 #define UDMA_BITS_PER_LONG_SHIFT 6
@@ -58,6 +59,22 @@ struct udma_u_db_page {
 	uint32_t bitmap_cnt;
 };
 
+struct udma_u_hugepage_priv {
+	void *va_base;
+	uint32_t va_len;
+	uint32_t left_va_offset;
+	uint32_t left_va_len;
+	uint32_t refcnt;
+	struct udma_u_hugepage_priv *pre;
+	struct udma_u_hugepage_priv *next;
+};
+
+struct udma_u_hugepage {
+	void *va_start;
+	uint32_t va_len;
+	struct udma_u_hugepage_priv *priv;
+};
+
 struct udma_u_context {
 	urma_context_t		urma_ctx;
 	void			*db_addr;
@@ -74,9 +91,13 @@ struct udma_u_context {
 	bool			dump_aux_info;
 	uint32_t		jfr_sge;
 	struct node_tbl		src_idx_tbl[UDMA_U_TBL_NUM];
+	bool			hugepage_enable;
+	pthread_mutex_t		hugepage_lock;
+	struct udma_u_hugepage_priv *hugepage_list;
 };
 
 struct udma_u_jetty_queue {
+	struct udma_u_context *ctx;
 	/* Command queue */
 	void *qbuf; /* Base virtual address of command buffer */
 	void *qbuf_end;
@@ -103,6 +124,7 @@ struct udma_u_jetty_queue {
 	bool lock_free;
 	bool cstm; /* sq ctrl flag */
 	struct udma_u_hmap_node hmap_node;
+	struct udma_u_hugepage *hugepage;
 };
 
 struct udma_wqe_sge {

src/urma/hw/udma/udma_u_ctl.c
@@ -90,14 +90,23 @@ static int udma_u_init_jfs_queue_buf(struct udma_u_jetty_queue *q,
 
 	if (cstm) {
 		q->qbuf = cfg_ex->cstm_cfg.sq.buff;
+	} else if (q->ctx->hugepage_enable) {
+		q->hugepage = udma_u_alloc_hugepage(q->ctx, q->qbuf_size);
+		if (q->hugepage) {
+			q->qbuf = q->hugepage->va_start;
+		} else {
+			UDMA_LOG_WARN("failed to alloc hugepage buf, switch to alloc standard buf.");
+			q->qbuf = udma_u_alloc_buf(q->qbuf_size);
+		}
 	} else {
 		q->qbuf = udma_u_alloc_buf(q->qbuf_size);
-		if (q->qbuf == NULL) {
-			UDMA_LOG_ERR("failed to alloc queue buffer.\n");
-			free(q->wrid);
-			q->wrid = NULL;
-			return ENOMEM;
-		}
+	}
+
+	if (q->qbuf == NULL) {
+		UDMA_LOG_ERR("failed to alloc queue buffer.\n");
+		free(q->wrid);
+		q->wrid = NULL;
+		return ENOMEM;
 	}
 
 	q->qbuf_curr = q->qbuf;
@@ -377,6 +386,7 @@ static urma_jfs_t *udma_u_create_jfs_ex(urma_context_t *ctx,
 		return NULL;
 	}
 
+	jfs->sq.ctx = udma_ctx;
 	if (udma_u_create_sq_ex(&jfs->sq, cfg_ex)) {
 		UDMA_LOG_ERR("failed to create sq.\n");
 		goto err_create_sq;
@@ -537,6 +547,7 @@ static urma_jetty_t *udma_u_create_jetty_ex(urma_context_t *ctx, struct udma_u_j
 		goto err_init_jetty_trans_mode;
 
 	udma_u_init_jfs_cfg_ex(cfg_ex, &jfs_cfg);
+	jetty->sq.ctx = udma_ctx;
 	ret = udma_u_create_sq_ex(&jetty->sq, &jfs_cfg);
 	if (ret != 0) {
 		UDMA_LOG_ERR("jetty create sq failed, ret = %d.\n", ret);

src/urma/hw/udma/udma_u_jetty.c
@@ -39,6 +39,7 @@ int exec_jetty_create_cmd(urma_context_t *ctx, struct udma_u_jetty *jetty,
 	cmd.pi_type = jetty->pi_type;
 	cmd.jetty_type = jetty->jetty_type;
 	cmd.non_pin = jetty->sq.cstm;
+	cmd.is_hugepage = jetty->sq.hugepage != NULL;
 
 	udma_u_set_udata(&udata, &cmd, (uint32_t)sizeof(cmd), NULL, 0);
 	ret = urma_cmd_create_jetty(ctx, &jetty->base, cfg, &udata);
@@ -140,6 +141,7 @@ urma_jetty_t *udma_u_create_jetty(urma_context_t *ctx, urma_jetty_cfg_t *cfg)
 		goto err_add_to_grp;
 	}
 
+	jetty->sq.ctx = udma_ctx;
 	ret = udma_u_create_sq(&jetty->sq, &cfg->jfs_cfg);
 	if (ret) {
 		UDMA_LOG_ERR("Jetty create sq failed.\n");

src/urma/hw/udma/udma_u_jfc.c
@@ -46,17 +46,51 @@ static int udma_u_jfc_cmd(urma_context_t *ctx, struct udma_u_jfc *jfc,
 	cmd.buf_addr = (uintptr_t)jfc->cq.qbuf;
 	cmd.db_addr = (uintptr_t)jfc->sw_db;
 	cmd.buf_len = jfc->cq.qbuf_size;
+	cmd.is_hugepage = jfc->cq.hugepage != NULL;
 
 	udma_u_set_udata(&udata, &cmd, sizeof(cmd), NULL, 0);
 
 	return urma_cmd_create_jfc(ctx, &jfc->base, cfg, &udata);
 }
 
+static int udma_u_create_cq(struct udma_u_jetty_queue *cq, urma_jfc_cfg_t *cfg)
+{
+	uint32_t depth;
+
+	cq->lock_free = cfg->flag.bs.lock_free;
+	if (!cq->lock_free &&
+	    pthread_spin_init(&cq->lock, PTHREAD_PROCESS_PRIVATE)) {
+		UDMA_LOG_ERR("failed to init lock.\n");
+		return EFAULT;
+	}
+
+	depth = cfg->depth < UDMA_U_MIN_JFC_DEPTH ? UDMA_U_MIN_JFC_DEPTH : cfg->depth;
+	if (!udma_u_alloc_queue_buf(cq, depth, cq->ctx->cqe_size, UDMA_HW_PAGE_SIZE, false)) {
+		UDMA_LOG_ERR("failed to alloc jfc wqe buf.\n");
+		goto err_alloc_buf;
+	}
+
+	return 0;
+
+err_alloc_buf:
+	if (!cq->lock_free)
+		(void)pthread_spin_destroy(&cq->lock);
+
+	return EFAULT;
+}
+
+static void udma_u_delete_cq(struct udma_u_jetty_queue *cq)
+{
+	udma_u_free_queue_buf(cq);
+
+	if (!cq->lock_free)
+		(void)pthread_spin_destroy(&cq->lock);
+}
+
 urma_jfc_t *udma_u_create_jfc(urma_context_t *ctx, urma_jfc_cfg_t *cfg)
 {
 	struct udma_u_context *udma_ctx = to_udma_u_ctx(ctx);
 	struct udma_u_jfc *jfc;
-	uint32_t depth;
 	int ret;
 
 	ret = udma_u_check_jfc_cfg(ctx, cfg);
@@ -69,19 +103,10 @@ urma_jfc_t *udma_u_create_jfc(urma_context_t *ctx, urma_jfc_cfg_t *cfg)
 		return NULL;
 	}
 
-	jfc->cq.lock_free = cfg->flag.bs.lock_free;
-
-	if (!jfc->cq.lock_free &&
-	    pthread_spin_init(&jfc->cq.lock, PTHREAD_PROCESS_PRIVATE)) {
-		UDMA_LOG_ERR("failed to init user udma jfc spinlock.\n");
-		goto err_init_lock;
-	}
-
-	depth = cfg->depth < UDMA_U_MIN_JFC_DEPTH ? UDMA_U_MIN_JFC_DEPTH : cfg->depth;
-	if (!udma_u_alloc_queue_buf(&jfc->cq, depth, udma_ctx->cqe_size,
-				    UDMA_HW_PAGE_SIZE, false)) {
-		UDMA_LOG_ERR("failed to alloc user jfc wqe buf.\n");
-		goto err_alloc_buf;
+	jfc->cq.ctx = udma_ctx;
+	if (udma_u_create_cq(&jfc->cq, cfg)) {
+		UDMA_LOG_ERR("failed to create cq.\n");
+		goto err_create_cq;
 	}
 
 	jfc->sw_db = (uint32_t *)udma_u_alloc_sw_db(udma_ctx, UDMA_JFC_TYPE_DB);
@@ -105,11 +130,8 @@ urma_jfc_t *udma_u_create_jfc(urma_context_t *ctx, urma_jfc_cfg_t *cfg)
 err_create_jfc:
 	udma_u_free_sw_db(udma_ctx, jfc->sw_db, UDMA_JFC_TYPE_DB);
 err_alloc_sw_db:
-	udma_u_free_queue_buf(&jfc->cq);
-err_alloc_buf:
-	if (!jfc->cq.lock_free)
-		(void)pthread_spin_destroy(&jfc->cq.lock);
-err_init_lock:
+	udma_u_delete_cq(&jfc->cq);
+err_create_cq:
 	free(jfc);
 	return NULL;
 }
@@ -421,26 +443,6 @@ static void dump_cqe_aux_info(urma_context_t *ctx, urma_cr_t *cr)
 	ret = udma_u_query_cqe_aux_info(ctx, &in, &out, 0);
 	if (ret)
 		UDMA_LOG_ERR("query cqe aux info failed, ret = %d.\n", ret);
-}
-
-static void dump_cqe_aux_info(urma_context_t *ctx, urma_cr_t *cr)
-{
-	struct udma_u_cqe_info_in info_in;
-	urma_user_ctl_out_t out = {};
-	urma_user_ctl_in_t in = {};
-	int ret;
-
-	info_in.status = cr->status;
-	info_in.s_r = cr->flag.bs.s_r;
-	in.addr = (uint64_t)&info_in;
-	in.len = sizeof(struct udma_u_cqe_info_in);
-
-	ret = udma_u_query_cqe_aux_info(ctx, &in, &out, 0);
-	if (ret)
-		UDMA_LOG_ERR("query cqe aux info failed, ret = %d.\n", ret);
-	if (cr->status != URMA_CR_SUCCESS && udma_ctx->dump_aux_info)
-		dump_cqe_aux_info(&udma_ctx->urma_ctx, cr);
-
 }
 
 static enum jfc_poll_state udma_u_poll_one(struct udma_u_context *udma_ctx,

src/urma/hw/udma/udma_u_jfr.c
@@ -84,6 +84,7 @@ int exec_jfr_create_cmd(urma_context_t *ctx, struct udma_u_jfr *jfr,
 	cmd.idx_len = jfr->idx_que.buf.length;
 	cmd.jetty_addr = (uintptr_t)&jfr->rq;
 	cmd.non_pin = jfr->rq.cstm;
+	cmd.is_hugepage = jfr->rq.hugepage != NULL;
 
 	udma_u_set_udata(&udata, &cmd, sizeof(cmd), &resp, sizeof(resp));
 	ret = urma_cmd_create_jfr(ctx, &jfr->base, cfg, &udata);
@@ -153,6 +154,7 @@ urma_jfr_t *udma_u_create_jfr(urma_context_t *ctx, urma_jfr_cfg_t *cfg)
 		goto err_alloc_idx;
 	}
 
+	udma_jfr->rq.ctx = udma_ctx;
 	if (udma_u_create_rq(udma_ctx, udma_jfr)) {
 		UDMA_LOG_ERR("failed to create jfr rqe buf.\n");
 		goto err_create_rq;

src/urma/hw/udma/udma_u_jfs.c
@@ -105,6 +105,7 @@ int udma_u_exec_jfs_create_cmd(urma_context_t *ctx, struct udma_u_jfs *jfs,
 	cmd.sqe_bb_cnt = jfs->sq.sqe_bb_cnt;
 	cmd.pi_type = jfs->pi_type;
 	cmd.non_pin = jfs->sq.cstm;
+	cmd.is_hugepage = jfs->sq.hugepage != NULL;
 	cmd.jetty_type = jfs->jfs_type;
 	cmd.jfs_id = jfs->sq.db.id;
 	udma_u_set_udata(&udata, &cmd, (uint32_t)sizeof(cmd), NULL, 0);
@@ -158,6 +159,7 @@ void udma_u_delete_sq(struct udma_u_jetty_queue *sq)
 
 urma_jfs_t *udma_u_create_jfs(urma_context_t *ctx, urma_jfs_cfg_t *cfg)
 {
+	struct udma_u_context *udma_ctx = to_udma_u_ctx(ctx);
 	struct udma_u_jfs *jfs;
 
 	if (cfg->trans_mode == URMA_TM_RC) {
@@ -171,6 +173,7 @@ urma_jfs_t *udma_u_create_jfs(urma_context_t *ctx, urma_jfs_cfg_t *cfg)
 		return NULL;
 	}
 
+	jfs->sq.ctx = udma_ctx;
 	if (udma_u_create_sq(&jfs->sq, cfg)) {
 		UDMA_LOG_ERR("failed to create sq.\n");
 		goto err_create_sq;

commit 14da2e6b1d95a0d40e417ba6db1d80bc9f75aa9f

ub: udma: Bugfix related to 2M hugepage.
driver inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/umdk/issues/IDF8EB
CVE: NA

---------------------------------------------------------

This patch support fix bug about 2M hugepage.

Fixes: 30ea7621dafb ("udma: support 2M hugepage")

Signed-off-by: Wei Qin <qinwei61@huawei.com>

src/urma/hw/udma/kernel_headers/udma_abi.h
@@ -6,9 +6,9 @@
 
 #include <linux/types.h>
 
-#define MAP_COMMAND_MASK 0xff
-#define MAP_INDEX_MASK 0xffffff
-#define MAP_INDEX_SHIFT 8
+#define MAP_COMMAND_MASK 0xf
+#define MAP_INDEX_MASK 0xfffffff
+#define MAP_INDEX_SHIFT 4
 
 #define UDMA_SEGMENT_ACCESS_GUARD (1UL << 5)
 
@@ -85,7 +85,7 @@ struct udma_create_jetty_ucmd {
 struct udma_create_jfc_ucmd {
 	__aligned_u64 buf_addr;
 	__u32 buf_len;
-	__u32 mode; /* 0: normal, 1: user STARS mode, 2: kernel STARS mode */
+	__u32 mode; /* 0: normal, 1: user stars, 2: kernel stars */
 	__aligned_u64 db_addr;
 	__u32 is_hugepage : 1;
 	__u32 rsv : 31;
@@ -112,9 +112,9 @@ struct udma_create_jfr_resp {
 };
 
 enum db_mmap_type {
+	UDMA_MMAP_HUGEPAGE,
 	UDMA_MMAP_JFC_PAGE,
 	UDMA_MMAP_JETTY_DSQE,
-	UDMA_MMAP_HUGEPAGE,
 };
 
 enum {

src/urma/hw/udma/udma_u_buf.c
@@ -130,7 +130,7 @@ static void udma_u_hugepage_del(struct udma_u_context *ctx,
 static struct udma_u_hugepage_priv *
 udma_u_alloc_hugepage_priv(struct udma_u_context *ctx, uint32_t len)
 {
-	off_t offset = get_mmap_offset(0, ctx->page_size, UDMA_MMAP_HUGEPAGE);
+	off_t offset = get_mmap_offset((UDMA_HUGEPAGE_SIZE / ctx->page_size >> MAP_INDEX_SHIFT), ctx->page_size, UDMA_MMAP_HUGEPAGE);
 	struct udma_u_hugepage_priv *priv;
 	int ret;
 